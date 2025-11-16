aaf6c2baa5faed29e54fe0a0e9b5d9a6efe355cb
Daniil Kartsev <Priestess.k12@icloud.com>
2025-11-16 23:14:01 +0300

    Create Bisharp.java

---
+++ Bisharp.java
@@ -0,0 +1,13 @@
+package info.pokemons;
+
+import ru.ifmo.se.pokemon.*;
+import info.moves.*;
+
+public class Bisharp extends Pokemon {
+    public Bisharp(String name, int level) {
+        super(name, level);
+        setStats(65, 125, 100, 60, 70, 70);
+        setType(Type.DARK, Type.STEEL);
+        setMove(new NightSlash(), new ScaryFace(), new MetalSound(), new StoneEdge());
+    }
+}
