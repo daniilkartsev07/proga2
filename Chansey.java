e32fa7c613dceaf710e6a3794b67c00b1b736b6c
Daniil Kartsev <Priestess.k12@icloud.com>
2025-11-16 23:15:07 +0300

    Create Chansey.java

---
+++ Chansey.java
@@ -0,0 +1,13 @@
+package info.pokemons;
+
+import ru.ifmo.se.pokemon.*;
+import info.moves.*;
+
+public class Chansey extends Pokemon {
+    public Chansey(String name, int level) {
+        super(name, level);
+        setStats(250, 5, 5, 35, 105, 50);
+        setType(Type.NORMAL);
+        setMove(new WorkUp(), new Psychic(), new MudBomb());
+    }
+}
