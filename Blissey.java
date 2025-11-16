54b2c8cad69479fa9fadb3eed153a4fecdb46b98
Daniil Kartsev <Priestess.k12@icloud.com>
2025-11-16 23:15:28 +0300

    Create Blissey.java

---
+++ Blissey.java
@@ -0,0 +1,13 @@
+package info.pokemons;
+
+import ru.ifmo.se.pokemon.*;
+import info.moves.*;
+
+public class Blissey extends Pokemon {
+    public Blissey(String name, int level) {
+        super(name, level);
+        setStats(255, 10, 10, 75, 135, 55);
+        setType(Type.NORMAL);
+        setMove(new WorkUp(), new Psychic(), new MudBomb(), new ThunderWave());
+    }
+}
