81ade7f404ab3c9b79ca632cf3f48bd01eb8698f
Daniil Kartsev <Priestess.k12@icloud.com>
2025-11-29 11:28:03 +0300

    Update Blissey.java

--- Blissey.java
+++ Blissey.java
@@ -3,7 +3,7 @@ package info.pokemons;
 import ru.ifmo.se.pokemon.*;
 import info.moves.*;
 
-public class Blissey extends Pokemon {
+public class Blissey extends Chansey {
     public Blissey(String name, int level) {
         super(name, level);
         setStats(255, 10, 10, 75, 135, 55);
