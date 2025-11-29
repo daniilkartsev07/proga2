4dd794b529e1f8b878177dae423c3b16613ac622
Daniil Kartsev <Priestess.k12@icloud.com>
2025-11-29 11:25:59 +0300

    Update Chansey.java

--- Chansey.java
+++ Chansey.java
@@ -3,7 +3,7 @@ package info.pokemons;
 import ru.ifmo.se.pokemon.*;
 import info.moves.*;
 
-public class Chansey extends Pokemon {
+public class Chansey extends Happiny {
     public Chansey(String name, int level) {
         super(name, level);
         setStats(250, 5, 5, 35, 105, 50);
